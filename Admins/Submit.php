650031b5315988d357655e3479d1fe42fa9fad92
NedNotta <32251222+NedNotta@users.noreply.github.com>
2018-03-20 09:28:24 -0400

    Create Submit.php

---
+++ Admins/Submit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Submit Form</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+  </head>
+  <body>
+    <p>
+      Welcome <?php echo $_POST['user_name']; ?>
+      <br>
+      Your email is <?php echo $_POST['user_eamil']; ?.
+    </p>
+  </body>
+</html>
